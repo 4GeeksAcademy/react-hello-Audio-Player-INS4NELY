9fe177fbba85330bf1e3544455561ba948bffea3
ff.mendieta <82422221+INS4NELY@users.noreply.github.com>
2025-04-30 13:51:40 +0000

    update archive

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import SongsPlayer from "./Songs";
-import Player from "./Player";
 
 const Home = () => {
 	return (
 		<div>
 			<SongsPlayer/>
-			<Player/>
 		</div>
 	);
 };
